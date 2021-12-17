0d577cfab0d51a0ad6b5b572746ec7ee5d7dcec0
yumicode <75518128+yumicode@users.noreply.github.com>
2021-12-17 17:23:15 +0100

    First commit add all files

---
+++ main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+"use strict"
+
+//Déclarer des variables
+let n = 5;
+// Déclarations multiples
+let a = 1, b = 2, c;
+
+console.log("Bla bla");
+n = 1;
+n *= 2;
+console.log(n);
