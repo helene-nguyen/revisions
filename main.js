5670b508bdb98a1d129ade8d89fc6205ec6a23d7
yumicode <75518128+yumicode@users.noreply.github.com>
2022-01-04 15:02:02 +0100

    Add main.js file

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 // JavaScript Document
 "use strict"
 
-//Déclarer des variables
-let n = 5;
-// Déclarations multiples
-let a = 1, b = 2, c;
 
-console.log("Bla bla");
-n = 1;
-n *= 2;
-console.log(n);
+function writeText() {
+
+var welcome = "Hello" ;
+var myName = prompt("Who are you ?");
+
+document.getElementById("title").innerHTML = welcome + " " + myName + " !";  
+
+}
+
+writeText();
+
